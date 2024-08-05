3d9c7b911c2c5d64da77726c54780d273409e856
Yoad <yoad@wescover.com>
2024-08-05 09:52:45 +0300

    Scaffold the web client app

---
+++ web_client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex h-screen w-full flex-row justify-center bg-slate-400">
+      <div className="m-5 flex w-full flex-col items-center justify-around border-2 border-black bg-slate-200">
+        <div>To Be Reital App</div>
+        <div>Here be dragons</div>
+
+        <p className="text-3xl font-bold underline">Nothing Yet Here As Well</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
