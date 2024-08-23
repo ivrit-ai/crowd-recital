948196cb8fccf7ee1a7874b45110cc57d3594b48
Yoad <yoad@wescover.com>
2024-08-23 14:45:14 +0300

    Ensure SSL certs are optional for local Vite dev server

--- web_client/vite.config.ts
+++ web_client/vite.config.ts
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import fs from "fs";
 
+const sslKeyFile = path.join(__dirname, "../cert/key.pem");
+const sslCertFile = path.join(__dirname, "../cert/cert.pem");
+
+const keyAndCertFilesExists =
+  fs.existsSync(sslKeyFile) && fs.existsSync(sslCertFile);
+
+if (!keyAndCertFilesExists) {
+  console.warn(
+    "SSL key and cert files not found. Local Dev server will not server over HTTPS.",
+  );
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -12,10 +24,12 @@ export default defineConfig({
     },
   },
   server: {
-    https: {
-      key: fs.readFileSync(path.join(__dirname, "../cert/key.pem")),
-      cert: fs.readFileSync(path.join(__dirname, "../cert/cert.pem")),
-    },
+    https: keyAndCertFilesExists
+      ? {
+          key: fs.readFileSync(path.join(__dirname, "../cert/key.pem")),
+          cert: fs.readFileSync(path.join(__dirname, "../cert/cert.pem")),
+        }
+      : undefined,
     proxy: {
       "/api": {
         target: "https://127.0.0.1:8000",
