55cf78cc2bc003bc76768ef9ebc85cad442b5d8e
Yoad <yoad@wescover.com>
2024-08-26 21:17:59 +0300

    Silence some warnings and logs from the Stanza loading flow

--- server/engines/nlp_pipeline.py
+++ server/engines/nlp_pipeline.py
@@ -1,10 +1,10 @@
-from stanza import Pipeline
+from stanza import Pipeline, DownloadMethod
 
 
 class NlpPipeline:
     def __init__(self):
         self.lang = "he"
-        self.nlp = Pipeline(lang=self.lang, processors="tokenize,mwt")
+        self.nlp = Pipeline(lang=self.lang, processors="tokenize,mwt", download_method=DownloadMethod.REUSE_RESOURCES)
 
     def get_pipeline(self) -> Pipeline:
         return self.nlp
