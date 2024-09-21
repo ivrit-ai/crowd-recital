6d66dcab95af778ece5e56af6ca1ff48fcf437aa
Yoad <1684709+yoadsn@users.noreply.github.com>
2024-09-21 14:22:57 +0300

    Session Preview/Delete translation

--- web_client/src/components/SessionPreview/index.tsx
+++ web_client/src/components/SessionPreview/index.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import {
   ForwardedRef,
   forwardRef,
@@ -5,8 +6,9 @@ import {
   useEffect,
   useState,
 } from "react";
+import { useTranslation } from "react-i18next";
+
 import { getSessionPreviewOptions } from "@/client/queries/sessions";
-import { useQuery } from "@tanstack/react-query";
 
 type Props = {
   id: string;
@@ -17,6 +19,7 @@ const SessionPreview = (
   { id, onClose }: Props,
   ref: ForwardedRef<HTMLDialogElement>,
 ) => {
+  const { t } = useTranslation("recordings");
   const {
     data: sessionPreviewData,
     isPending,
@@ -31,13 +34,13 @@ const SessionPreview = (
   }, []);
 
   if (isError) {
-    return <div>ארעה שגיאה בהצגת הסשן</div>;
+    return <div>{t("same_clear_moth_fond")}</div>;
   }
 
   return (
     <dialog className="modal" ref={ref} onClose={onClose}>
       <div className="modal-box">
-        <h1 className="text-md">השמעת הקלטה - {id}</h1>
+        <h1 className="text-md">{t("silly_moving_eagle_fold", {id })}</h1>
         {!isPending && !playbackError ? (
           <video
             className="medium-captions my-4 h-[300px] w-[40rem] max-w-full border border-solid"
@@ -51,7 +54,7 @@ const SessionPreview = (
               onError={onPlaybackError}
             />
             <track
-              label="כתוביות"
+              label={t("mild_white_tapir_lift")}
               kind="subtitles"
               srcLang="he"
               src={sessionPreviewData!.transcript_url}
@@ -62,7 +65,7 @@ const SessionPreview = (
           <div className="flex min-h-[250px] w-full items-center justify-center">
             {playbackError && !isPending ? (
               <div className="alert alert-warning text-center text-lg">
-                ישנה בעיה בהשמעת ההקלטה
+                {t("lazy_tasty_piranha_tear")}
               </div>
             ) : (
               <div className="loading loading-infinity loading-lg" />
@@ -71,7 +74,7 @@ const SessionPreview = (
         )}
         <div className="modal-action justify-center">
           <button className="btn btn-primary btn-sm" onClick={() => onClose()}>
-            סגור
+            {t("full_merry_halibut_treat")}
           </button>
         </div>
       </div>
