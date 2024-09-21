64415d30052b88d2869dc2e4a993ced589cefcf3
Yoad <1684709+yoadsn@users.noreply.github.com>
2024-09-21 15:29:07 +0300

    Add yidish tranlsations for fun

--- web_client/src/i18n.ts
+++ web_client/src/i18n.ts
@@ -3,6 +3,9 @@ import { initReactI18next } from "react-i18next";
 import he from "./locales/he/translation.json";
 import heDocuments from "./locales/he/documents.json";
 import heRecordings from "./locales/he/recordings.json";
+import yi from "./locales/yi/translation.json";
+import yiDocuments from "./locales/yi/documents.json";
+import yiRecordings from "./locales/yi/recordings.json";
 
 export const defaultNS = "translation";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -11,13 +14,18 @@ export type TObj = any;
 i18n.use(initReactI18next).init({
   debug: true,
   defaultNS,
-  lng: "he",
+  lng: "yi",
   fallbackLng: "he",
   resources: {
     he: {
       translation: he,
       documents: heDocuments,
       recordings: heRecordings,
+    },
+    yi: {
+      translation: yi,
+      documents: yiDocuments,
+      recordings: yiRecordings,
     },
   },
 });
